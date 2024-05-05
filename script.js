6f8d5da48d7bf3540d0b32815649b671f31477c8
Casey Meyvn <shatternight@protonmail.com>
2024-05-05 18:18:55 +0200

    Make basic functionailty

---
+++ script.js
@@ -0,0 +1,38 @@
+let element = document.getElementById('word');
+let written = document.getElementById('written');
+
+let show_start = true;
+let writing = true;
+let word_count = 0;
+let words = [];
+
+document.addEventListener("keydown", function (event) {
+	if (writing) {
+		const key_name = event.key;
+		if (key_name === ' ' || key_name === 'Enter') {
+			words.push(element.innerHTML);
+			word_count += 1;
+			element.innerHTML = '';
+		}
+		else if (/^[a-zæøå]+$/.test(key_name)) {
+			if (show_start) {
+				element.innerHTML = '';
+				show_start = false;
+			}
+			element.innerHTML += key_name;
+		} else if (key_name === 'End') {
+			let date_now = new Date();
+			words.push(element.innerHTML);
+			word_count += 1;
+			element.innerHTML = '';
+			written.innerHTML = date_now.getFullYear() + '-' + (date_now.getMonth() + 1) + '-' + date_now.getDate() + '<br><br>';
+			words.forEach(word => {
+				written.innerHTML += word + ' ';
+			});
+			writing = false;
+		} else if (key_name === 'Backspace') {
+			let line = element.innerHTML;
+			element.innerHTML = line.slice(0, -1);
+		}
+	}
+});
